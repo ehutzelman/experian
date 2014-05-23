aa1683e695858cb4a2199c238f59c61e6a5f1f5c
Kelsey Mok <kelseymok@gmail.com>
2014-05-23 16:03:33 -0500

    xml-version: changed xml version from 5.0 to 05 to resolve Experian Error 720 validation, as it requires that tag to be 2 char

--- lib/experian/precise_id/secondary_request.rb
+++ lib/experian/precise_id/secondary_request.rb
@@ -4,7 +4,7 @@ module Experian
 
       def add_request_content(xml)
         xml.tag!('KBAAddOn') do
-          xml.tag!('XMLVersion', "5.0")
+          xml.tag!('XMLVersion', "05")
           xml.tag!('OutWalletRequestData') do
             xml.tag!('SessionID', @options[:session_id])
             xml.tag!('OutWalletQuestionsRequest', "Y")
