e778cee1044448ff981e2c242eee9df219864b9d
Kelsey Mok <kelseymok@gmail.com>
2014-05-23 16:40:17 -0500

    xml-version: removes xml request namespace and version from the request tag

--- lib/experian/request.rb
+++ lib/experian/request.rb
@@ -12,7 +12,7 @@ module Experian
       xml = Builder::XmlMarkup.new(:indent => 2)
       xml.instruct!(:xml, :version => '1.0', :encoding => 'utf-8')
       xml.tag!("NetConnectRequest",
-        #:xmlns => Experian::XML_NAMESPACE,
+        :xmlns => Experian::XML_NAMESPACE,
         'xmlns:xsi' => Experian::XML_SCHEMA_INSTANCE,
         'xsi:schemaLocation' => Experian::XML_SCHEMA_LOCATION) do
           yield xml if block_given?
